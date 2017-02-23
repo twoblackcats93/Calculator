d670f971715a68cb9349e338f3b0e6f82b494cac
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-22 18:07:19 -0800

    commented calc fxn

---
+++ our_arithmetic.py
@@ -0,0 +1,34 @@
+def add(num1, num2):
+    """Return the sum of two numbers"""
+    return num1 + num2
+
+def subtract(num1, num2):
+    """Return the difference of two numbers"""
+    return num1 - num2
+
+def multiply(num1, num2):
+    """Return the product of two numbers"""
+    return num1 * num2
+
+def divide(num1, num2):
+    """Return the quotient of two numbers as a float"""
+    return float(num1) / num2
+
+def square(num):
+    """Return the square of a number"""
+    return num**2
+
+def cube(num):
+    """Return the cube of a number"""
+    return num**3
+
+def power(num, exponent):
+    """Return num raised to the power of exponent"""
+    return num**exponent
+
+def mod(num1, num2):
+    """Return remainder of num1 divided by num2"""
+    return num1 % num2
+
+
+    
