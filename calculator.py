636beced85ad168278b3814965c3cfd02b298e9a
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-22 18:02:27 -0800

    Added the square and cube operations

--- calculator.py
+++ calculator.py
@@ -5,43 +5,48 @@ Using our arithmetic.py file from Exercise02, create the
 calculator program yourself in this file.
 """
 
-list1 = [1, 2, 3]
-
 from our_arithmetic import *
 
 # add print statement of user equation options like + - pow and in prefix style
 def user_input():
-    user_equation = raw_input("Enter the operation followed by the numbers you'd like to perform: ")
-    tokenize_equation = user_equation.split(" ")
-    operator = tokenize_equation[0]
-    num1 = int(tokenize_equation[1])
-    num2 = int(tokenize_equation[2])
-
 
     while True:
-        if tokenize_equation[0] == "+":
-            print add(num1, num2)
+        # get user input and tokenize it
+        user_equation = raw_input("Enter the operation followed by the numbers you'd like to perform: ")
+        tokens = user_equation.split(" ")
+        operator = tokens[0]
+        
+        # enables user to quit
+        if operator == "q":
+            break
 
-        if tokenize_equation[0] == "-":
-            print subtract(num1, num2)
+        # enables user to use the arithmetic functions  
+        if operator == "+":
+            print add(int(tokens[1]), int(tokens[2]))
 
-        if tokenize_equation[0] == "*":
-            print multiply(num1, num2)
+        elif operator == "-":
+            print subtract(int(tokens[1]), int(tokens[2]))
 
-        if tokenize_equation[0] == "/":
-            print divide(num1, num2)        
+        elif operator == "*":
+            print multiply(int(tokens[1]), int(tokens[2]))
 
-        if tokenize_equation[0] == "%":
-            print mod(num1, num2)
+        elif operator == "/":
+            print divide(int(tokens[1]), int(tokens[2]))        
 
-        if tokenize_equation[0] == "**":
-            print power(num1, num2)
+        elif operator == "%":
+            print mod(int(tokens[1]), int(tokens[2]))
+
+        elif operator == "**":
+            print power(int(tokens[1]), int(tokens[2]))
             
-        # if tokenize_equation[0] == "+":
-        #     print add(num1, num2)
+        elif operator == "**2":
+            print square(int(tokens[1]))
 
-        # if tokenize_equation[0] == "+":
-        #     print add(num1, num2) 
+        elif operator == "**3":
+            print cube(int(tokens[1])) 
+
+        else:
+            print "You've entered an invalid operation."
 
 user_input()
 
